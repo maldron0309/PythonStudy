69b31f8940b84162f23a77eb5df84c05a12dde60
maldron <bagjinhyeong640@gmail.com>
2023-10-10 09:50:53 +0900

    a

--- 1.py
+++ 1.py
@@ -45,4 +45,32 @@ print(str1[:2])
 print(str1[1:-1])
 
 
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# 
+
+
+print("박진형" *5)
+
+for i in range(5) :
+  print(i+1,": 박진형")
+print("END")
+
+range(5)
+
+print(range(5))
+ls = list(range(5))
+ls
+
+ls = list(range(1,6))
+ls
+
+ls = list(range(10,101,10))
+ls
+
+ls = list(range(10,0,-1))
+print(ls)
+
 
